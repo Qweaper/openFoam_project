331f15d849ad2041a1f63668c1adfc8b8c46e20a
Vlad <reaperzaz@gmail.com>
2023-12-01 16:43:34 +0300

    initial commit

---
+++ kursovaya/dynamicCode/_85e7620eff332bca6fb2b5587b921d66fe4a3dae/codeStreamTemplate.C
@@ -0,0 +1,75 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) YEAR OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Template for use with codeStream.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dictionary.H"
+#include "fieldTypes.H"
+#include "Ostream.H"
+#include "Pstream.H"
+#include "read.H"
+#include "unitConversion.H"
+
+//{{{ begin codeInclude
+
+//}}} end codeInclude
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    void codeStream_85e7620eff332bca6fb2b5587b921d66fe4a3dae
+    (
+        Ostream& os,
+        const dictionary& dict
+    )
+    {
+//{{{ begin code
+        #line 1 "/home/vlad/Documents/polytech/analisys_systems/kursach/kursovaya/system/blockMeshDict"
+os << (-dict.lookupScoped<label>("pos_half_w", true, false));
+//}}} end code
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
